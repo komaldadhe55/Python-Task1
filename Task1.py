8a706b71307e1ecdcf0a3ce32723652759e4da4e
komaldadhe55 <komaldadhe6@gmail.com>
2026-08-04 22:43:17 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,35 @@
+print("Task 1 : Identity Card")
+
+print("------- My ID Card -------")
+print("Name : Komal V. Dadhe")
+print("Age : 22")
+print("Date of Birth : 05/01/2004")
+print("Gender : Female")
+print("Blood Group : O+")
+print("Address : Gajanan nagar, pimpalgaon road,Yavatmal")
+print("City : Yavatmal")
+print("College Name : College of Management And Computer Science, Yavatmal")
+print("Mobile No. : 7499638455")
+print("Email : komaldadhe6@gmail.com")
+
+print("Task 2 : Find data type")
+
+a=25
+b=25.5
+c="Twenty Five"
+d=True
+e=25+3j
+print("Data type of a:", type(a))
+print("Data type of b:", type(b))
+print("Data type of c:", type(c))
+print("Data type of d:", type(d))
+print("Data type of e:", type(e))
+
+print("Task 3 : Memory Detection")
+
+v1=100
+v2=200
+v3=100
+print("Memory address of v1:", id(v1))
+print("Memory address of v2:", id(v2))
+print("Memory address of v3:", id(v3))
